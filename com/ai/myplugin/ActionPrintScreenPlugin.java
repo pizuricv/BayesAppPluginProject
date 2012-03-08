17788a1bcdb5ad4d77272d697d9f8c112dc8f3ca
Veselin Pizurica <veselin.pizurica@gmail.com>
2012-03-08 23:25:40 +0100

    First commit

---
+++ com/ai/myplugin/ActionPrintScreenPlugin.java
@@ -0,0 +1,77 @@
+/**
+ * User: Veselin Pizurica
+ */
+
+package com.ai.myplugin;
+
+import com.ai.bayes.scenario.Receiver;
+import com.ai.bayes.scenario.ActionResult;
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+import com.ai.bayes.plugins.BNActionPlugin;
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.System;
+
+@PluginImplementation
+public class ActionPrintScreenPlugin implements BNActionPlugin{
+
+    private static final String DUMMY_PROPERTY = "dummy property that will be print out";
+    Map<String, Object> propertiesMap = new HashMap<String, Object>();
+
+    @Override
+    public String[] getRequiredProperties() {
+        return new String[]{DUMMY_PROPERTY};
+    }
+
+    @Override
+    public void setProperty(String string, Object obj) {
+        if(string.equals(DUMMY_PROPERTY)) {
+            propertiesMap.put(string,obj);
+        }
+    }
+
+    @Override
+    public Object getProperty(String string) {
+        return propertiesMap.get(string);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Action that only prints the event";
+    }
+
+    @Override
+    public BNActionPlugin getNewInstance() {
+        return new ActionPrintScreenPlugin();
+    }
+
+    @Override
+    public ActionResult action(Receiver receiver) {
+        if(propertiesMap.containsKey(DUMMY_PROPERTY)){
+            System.out.println("###########DUMMY ACTION#######"+ propertiesMap.get(DUMMY_PROPERTY));
+        }  else{
+            System.out.println("###########DUMMY ACTION#######");
+        }
+
+        if(receiver != null){
+            return receiver.action();
+        }
+        
+        return new ActionResult() {
+            @Override
+            public boolean isSuccess() {
+                return true;
+            }
+
+            @Override
+            public String getObserverState() {
+                return null;
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "Print Screen Action V1";
+    }
+}
---
+++ com/ai/myplugin/PingSensorV1.java
@@ -0,0 +1,129 @@
+/**
+ * created by: Veselin Pizurica
+ * Date: 06/03/12
+ */
+
+package com.ai.myplugin;
+
+import com.ai.bayes.scenario.TestResult;
+import com.ai.bayes.model.BayesianNetwork;
+import com.ai.util.resource.TestSessionContext;
+import com.ai.bayes.plugins.BNSensorPlugin;
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.System;
+
+
+@PluginImplementation
+public class PingSensorV1 implements BNSensorPlugin{
+    private static final String IP_ADDRESS = "IP address";
+    private static final String TIMEOUT = "timeout";
+    private static final String ALIVE = "Alive";
+    private static final String NOT_ALIVE = "Not Alive";
+    private String nodeName;
+
+    Map<String, Object> propertiesMap = new HashMap<String, Object>();
+
+
+    public String[] getRequiredProperties() {
+        return new String[] {IP_ADDRESS, TIMEOUT};
+    }
+
+    public void setProperty(String string, Object obj) {
+        if(string.equals(IP_ADDRESS)) {
+            try {
+                propertiesMap.put(string,InetAddress.getByName(obj.toString()));
+            } catch (UnknownHostException e) {
+                System.err.println(e.getLocalizedMessage());
+            }
+        } else if(string.equals(TIMEOUT)){
+            propertiesMap.put(string, Integer.parseInt(obj.toString()));
+        }
+    }
+
+    public Object getProperty(String string) {
+        return propertiesMap.get(string);
+    }
+
+    public String getDescription() {
+        return "Ping test V1 to check IP connectivity";
+    }
+
+    public TestResult execute(TestSessionContext testSessionContext) {
+        boolean reachable = false;
+        boolean testSuccess = true;
+
+        try {
+            reachable = getAddress().isReachable(getTimeOut());
+        } catch (IOException e) {
+            testSuccess = false;
+            System.err.println(e.getLocalizedMessage());
+        }
+        final boolean finalTestFailed = testSuccess;
+        final boolean finalReachable = reachable;
+        TestResult result = new TestResult() {
+            public boolean isSuccess() {
+                return finalTestFailed;
+            }
+            /*
+            you need to return the node name, since the diagnosis result for the node is linked to the node name of the test result 
+            */
+            public String getName() {
+                return getNodeName();
+            }
+
+            public String getObserverState() {
+                if(finalReachable){
+                    return ALIVE;
+                } else {
+                    return NOT_ALIVE;
+                }
+            }
+        };
+        return result;
+    }
+
+    /*
+    Name needs to be unique across different sensors
+    */
+    public String getName() {
+        return "Ping Test V1";
+    }
+
+    public String[] getSupportedStates() {
+        return new String[] {ALIVE, NOT_ALIVE} ;
+    }
+
+    private InetAddress getAddress(){
+        return (InetAddress) getProperty(IP_ADDRESS);
+    }
+
+    private int getTimeOut(){
+        return (Integer) getProperty(TIMEOUT);
+    }
+
+    public BNSensorPlugin getNewInstance() {
+        return new PingSensorV1();
+    }
+
+    public void setNodeName(String nodeName) {
+        this.nodeName = nodeName;
+    }
+
+    public String getNodeName() {
+        return nodeName;
+    }
+
+    /*
+    Only if you need the access to the belief network, otherwise you can don't need this call
+    */
+    public void setBayesianNetwork(BayesianNetwork bayesianNetwork) {
+    }
+
+
+}
