ceac940edf14a8893869da2bf497499c84c89682
Veselin Pizurica <veselin.pizurica@gmail.com>
2013-09-09 20:39:28 +0200

    Changed the property name of the Date sensor

---
+++ src/main/java/com/ai/myplugin/sensor/ScriptSensor.java
@@ -0,0 +1,11 @@
+package com.ai.myplugin.sensor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: veselin
+ * Date: 07/09/13
+ * Time: 21:09
+ * To change this template use File | Settings | File Templates.
+ */
+public class ScriptSensor {
+}
