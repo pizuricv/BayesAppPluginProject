889a9eca0a1511bd6d16f4d5d74ef616eb21931d
Veselin Pizurica <veselin.pizurica@gmail.com>
2013-09-10 23:38:05 +0200

    Added UV index sensor

---
+++ src/main/java/com/ai/myplugin/sensor/UVSensor.java
@@ -0,0 +1,11 @@
+package com.ai.myplugin.sensor;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: veselin
+ * Date: 10/09/13
+ * Time: 21:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class UVSensor {
+}
