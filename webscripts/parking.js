d4bc97cbb00d0495873d41a4758415960766dc9c
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-02-18 17:35:13 +0100

    webscripts

---
+++ webscripts/parking.js
@@ -0,0 +1,17 @@
+var request = require("request");
+var url = "http://datatank.gent.be/Mobiliteitsbedrijf/Parkings11.json";
+request({
+    url: url,
+    json: true
+}, function (error, response, body) {
+
+ if (!error && response.statusCode === 200) {
+    var parkings = {
+       observedState: "Found",
+       rawData : {
+         locations: body.Parkings11.parkings
+       }
+    };
+    console.log(JSON.stringify(parkings), null, 4);
+  }
+});
