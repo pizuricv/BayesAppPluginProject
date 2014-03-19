ab1f4846f64ff6f433dfa4cee7833cd1985729a6
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-03-19 11:10:20 +0100

    added more detailed info about air polution

--- src/main/java/com/ai/myplugin/sensor/AirQualitySensor.java
+++ src/main/java/com/ai/myplugin/sensor/AirQualitySensor.java
@@ -28,6 +28,7 @@ public class AirQualitySensor implements BNSensorPlugin{
     private static final Log log = LogFactory.getLog(AirQualitySensor.class);
     public static final String LOCATION = "location";
     String pathURL = "http://luchtkwaliteit.vmm.be/lijst.php";
+    String detailInfoIRCURL = "http://deus.irceline.be/~celinair/actair/actair.php?lan=nl";
     private String location = null;
 
     @Override
@@ -71,6 +72,13 @@ public class AirQualitySensor implements BNSensorPlugin{
 
         int value = -1;
         String stringToParse = "";
+        double O3 = -1;
+        double NO2 = -1;
+        double CO = -1;
+        double SO2 = -1;
+        double PM10 = -1;
+        double PM25 = -1;
+        double C6H6 = -1;
 
         try{
             stringToParse = Rest.httpGet(pathURL);
@@ -98,8 +106,46 @@ public class AirQualitySensor implements BNSensorPlugin{
             log.error("location not found");
             return new EmptyTestResult();
         }
+        //try to get more detail informatoin, don't fail if there is nothing
+        try{
+            stringToParse = Rest.httpGet(detailInfoIRCURL);
+            Document doc = Jsoup.parse(stringToParse);
+            for (Element table : doc.select("table")) {
+                for (Element row : table.select("tr")) {
+                    Elements tds = row.select("td");
+                    //index, location, O3, NO2, CO, SO2, PM10. PM2.5, C6H6
+                    if (tds.size() >8) {
+                        String location = tds.get(1).text();
+                        if(location.equalsIgnoreCase((String) getProperty(LOCATION))) {
+                            O3 = getDouble(tds.get(2).text().trim().replace(",", "."));
+                            NO2 = getDouble(tds.get(3).text().trim().replace(",", "."));
+                            CO = getDouble(tds.get(4).text().trim().replace(",", "."));
+                            SO2 = getDouble(tds.get(5).text().trim().replace(",", "."));
+                            PM10 = getDouble(tds.get(6).text().trim().replace(",", "."));
+                            PM25 = getDouble(tds.get(7).text().trim().replace(",", "."));
+                            C6H6 = getDouble(tds.get(8).text().trim().replace(",", "."));
+                            log.info("Found detailed entry " + tds.get(0).text() + ":" + tds.get(1).text() + ":" +
+                                    tds.get(2).text() + ":" + tds.get(3).text() + ":" + tds.get(4).text()+
+                                    tds.get(5).text() + ":" + tds.get(6).text() + ":" +
+                                    tds.get(7).text() + ":" + tds.get(8).text());
+                            break;
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            log.error(e.getLocalizedMessage());
+            e.printStackTrace();
+        }
 
         final int finalValue = value;
+        final double finalO3 = O3;
+        final double finalCO = CO;
+        final double finalNO = NO2;
+        final double finalPM1 = PM10;
+        final double finalSO = SO2;
+        final double finalPM2 = PM25;
+        final double finalC6H = C6H6;
         return new TestResult() {
             @Override
             public boolean isSuccess() {
@@ -125,9 +171,32 @@ public class AirQualitySensor implements BNSensorPlugin{
             public String getRawData() {
                 JSONObject jsonObject = new JSONObject();
                 jsonObject.put("value", finalValue);
+
+                if(finalO3 != -1)
+                    jsonObject.put("O3", finalO3);
+                if(finalCO != -1)
+                    jsonObject.put("CO", finalCO);
+                if(finalNO != -1)
+                    jsonObject.put("NO2", finalNO);
+                if(finalPM1 != -1)
+                    jsonObject.put("PM10", finalPM1);
+                if(finalSO != -1)
+                    jsonObject.put("SO2", finalSO);
+                if(finalPM2 != -1)
+                    jsonObject.put("PM25", finalPM2);
+                if(finalC6H != -1)
+                    jsonObject.put("C6H6", finalC6H);
                 return jsonObject.toJSONString();
             }
         };
+    }
+
+    private double getDouble(String value) {
+        try {
+            return Double.parseDouble(value);
+        } catch (Exception e){
+            return -1.;
+        }
     }
 
     @Override
