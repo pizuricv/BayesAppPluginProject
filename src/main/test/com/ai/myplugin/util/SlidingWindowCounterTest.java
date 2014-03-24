ea1153a9b588fc5cdc1e684c8811db11a2103c36
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-03-24 09:01:40 +0100

    added test for sliding counter

---
+++ src/main/test/com/ai/myplugin/util/SlidingWindowCounterTest.java
@@ -0,0 +1,38 @@
+package com.ai.myplugin.util;
+
+import junit.framework.TestCase;
+
+import java.util.Map;
+
+/**
+ * Created by User: veselin
+ * On Date: 24/03/14
+ */
+public class SlidingWindowCounterTest extends TestCase{
+
+    public void testIncrementCount() throws Exception {
+        SlidingWindowCounter<Long> slidingWindowCounter = new SlidingWindowCounter<Long>(2);
+        slidingWindowCounter.incrementCount(4l);
+        slidingWindowCounter.incrementCount(4l);
+        slidingWindowCounter.incrementCount(2l);
+        slidingWindowCounter.incrementCount(3l);
+        slidingWindowCounter.incrementCount(4l);
+        Map map1 = slidingWindowCounter.getCountsThenAdvanceWindow();
+        slidingWindowCounter.incrementCount(5l);
+        slidingWindowCounter.incrementCount(3l);
+        slidingWindowCounter.incrementCount(4l);
+        Map map2 = slidingWindowCounter.getCountsThenAdvanceWindow();
+
+        slidingWindowCounter.incrementCount(5l);
+        slidingWindowCounter.incrementCount(3l);
+        slidingWindowCounter.incrementCount(4l);
+        Map map3 = slidingWindowCounter.getCountsThenAdvanceWindow();
+
+        assertTrue("{2=1, 3=1, 4=3}".equals(map1.toString()));
+        assertTrue("{2=1, 3=2, 4=4, 5=1}".equals(map2.toString()));
+        assertTrue("{2=0, 3=2, 4=2, 5=2}".equals(map3.toString()));
+
+
+    }
+
+}
