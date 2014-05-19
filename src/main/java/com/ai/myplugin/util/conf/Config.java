0242c7776c0d0167f41c334d8d6193cb77a89755
Francis De Brabandere <francisdb@gmail.com>
2014-05-19 16:44:34 +0200

    also allow loading config from user home

--- src/main/java/com/ai/myplugin/util/conf/Config.java
+++ src/main/java/com/ai/myplugin/util/conf/Config.java
@@ -4,6 +4,7 @@ package com.ai.myplugin.util.conf;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,12 +18,21 @@ public final class Config {
 
     public static Configuration load(){
         Properties properties = new Properties();
-        try(InputStream is = new FileInputStream(CONFIG_FILE)) {
+        try(InputStream is = new FileInputStream(findConfigFile())) {
             properties.load(is);
         } catch (IOException e) {
             log.error(e.getLocalizedMessage(), e);
         }
         // TODO we might want to cache this and return a defensive copy?
         return new PropertiesConfiguration(properties);
+    }
+
+    // TODO we might want to also load it from the classpath
+    public static File findConfigFile(){
+        File file = new File(CONFIG_FILE);
+        if(!file.exists()){
+            file = new File(System.getProperty("user.home"), CONFIG_FILE);
+        }
+        return file;
     }
 }
