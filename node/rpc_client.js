6c846b8e10047d24180261423aabc94c24a36e0d
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-06-18 16:35:46 +0200

    update of the node with sensor plugin capablity

--- node/rpc_client.js
+++ node/rpc_client.js
@@ -13,12 +13,53 @@ var options = {
 
 // Create a server object with options
 var client = new rpc.Client(options);
-
+console.log('call 2: waylay_rpc');
 client.call (
-  {"jsonrpc": "2.0", "method": "waylay_rpc", "params": [new Buffer("var value = {observedState:\"hello\",rawData:{}};sandbox.send( value); ").toString('base64')], "id": 2},
+  {"jsonrpc": "2.0", "method": "waylay_rpc", "params": ["var error, value = {observedState:\"hello\",rawData:{}};sandbox.send(null,value);"], "id": 2},
+  function (err, res){
+    // Did it all work ?
+    if (err) { console.log(err); }
+    else { console.log(res); }
+  });
+console.log('call 3: register_sensor');
+client.call (
+  {"jsonrpc": "2.0", "method": "register_sensor", "params": ["sensorX", "var error, value = { observedState: \"hello\", rawData: \"hello2\"};sandbox.send(null, value);", {author
+  : "Veselin" } ], "id": 3},
+  function (err, res){
+    // Did it all work ?
+    if (err) { console.log(err); }
+    else { console.log(res); }
+  });
+
+setTimeout(function(){
+console.log('call 4: execute_sensor');
+client.call (
+  {"jsonrpc": "2.0", "method": "execute_sensor", "params": ["sensorX"], "id": 4},
+  function (err, res){
+    // Did it all work ?
+    if (err) { console.log(err); }
+    else { console.log(res); }
+  });
+}, 1000);
+
+console.log('call 5: info');
+client.call (
+  {"jsonrpc": "2.0", "method": "info", "params": [], "id": 5},
   function (err, res){
     // Did it all work ?
     if (err) { console.log(err); }
     else { console.log(res); }
   }
 );
+
+setTimeout(function(){
+console.log('call 6: sensors');
+client.call (
+  {"jsonrpc": "2.0", "method": "sensors", "params": [], "id": 6},
+  function (err, res){
+    // Did it all work ?
+    if (err) { console.log(err); }
+    else { console.log(res); }
+  }
+);
+}, 1500);
--- node/rpc_server.js
+++ node/rpc_server.js
@@ -1,8 +1,11 @@
 var rpc = require('node-json-rpc');
-var req = require("request");
-var vm = require('vm');
+var request = require('request');
+var cheerio = require('cheerio');
+var dust = require('dustjs-linkedin');
 var Promise = require('promise');
-sandbox = {request:req, console:console};
+var vm = require('vm');
+var fs = require('fs');
+sandbox = {cheerio:cheerio, request:request, console:console};
 var countTotal = 0;
 var errorTotal = 0;
 
@@ -17,44 +20,144 @@ path: '/',
 strict: true
 };
 
-// Create a server object with options
 var serv = new rpc.Server(options);
 
-serv.addMethod('waylay_rpc', function (para, callback) {
-  var error, result;
-  console.log("recieved request" );
-//IMPORTANT: script will use the callback to send the result to the sensor
-sandbox.send = callback;
-if (para.length === 1) {
+function handleError(err, code, callback) {
+  console.log("error " + err);
+  var result;
+  errorTotal ++;
+  error = { code: code, message: err.message};
+  callback(error, result);
+}
+
+function runScript (content, options, callback) {
+  countTotal ++;
+  console.log("runScript script: " + content);
+  sandbox.send = callback;
   var promise = new Promise(function(resolve, reject) {
     try {
-      var _script = new Buffer(para[0], 'base64').toString('ascii');
+     // var _script = new Buffer(content, 'base64').toString('ascii');
+     _script = content;
+      if(options){
+        console.log("compile options: " + options);
+        var compiled =  dust.compile(_script, "dust");
+        dust.loadSource(compiled);
+        dust.render("dust", options, function(err, out) {
+          console.log(out);
+          _script = out;
+        });
+      }
       console.log("executing script: " + _script);
       var script = vm.createScript(_script,'myfile.vm');
       script.runInThisContext(sandbox);
       console.log('script executed');
       resolve();
-    } catch(err){
-     reject(err);
-   }
- });
+      } catch(err){
+       reject(err);
+     }
+   });
   promise.then(function(result) {
-    countTotal ++;
     console.log("Total number of requests:" +countTotal + ", total number of errors " + errorTotal);
   }, function(err) {
-    errorTotal ++;
-    console.log(err);
-    error = { code: -32603, message: err.message };
-    callback(error, result);
-    console.log("Total number of requests:" +countTotal + ", total number of errors " + errorTotal);
+    handleError(err, -32603, callback);
   });
 }
-else {
-  error = { code: -32602, message: "Invalid params" };
-  errorTotal ++;
-  callback(error, result);
-  console.log("Total number of requests:" +countTotal + ", total number of errors " + errorTotal);
-}
+
+serv.addMethod('waylay_rpc', function (para, callback) {
+  countTotal ++;
+  console.log("recieved RPC call" );
+  if (para.length > 0) {
+    var name = para[0];
+    runScript(name, null, callback);
+  } else {
+    handleError(new Error("Invalid params "), 32602, callback);
+  }
+});
+
+//para[0] script name, para[1] options - using template
+serv.addMethod('execute_sensor', function(para, callback){
+  countTotal ++;
+  var error, result;
+  if (para.length >0) {
+    var name = para[0];
+    console.log("execute sensor: " + name);
+    var dataTemp, data;
+    try{
+      dataTemp = fs.readFileSync('./config.json');
+      data = JSON.parse(dataTemp);
+      var sensor = data[name];
+      if(sensor === "undefined"){
+        handleError(new Error("Invalid sensor name: " +name ), 32602, callback);
+      }
+      var content = data[name].script;
+      var options = para[1];
+      runScript(content, options, callback);
+    } catch(err){
+      handleError(err, -32602, callback);
+   }
+ }
+});
+
+//para[0]: sensor name, para[1]: script  content, para[2] metadata
+serv.addMethod('register_sensor', function(para, callback){
+  countTotal ++;
+  var error, result;
+  if (para.length === 3) {
+    try {
+      var name = para[0];
+      var script = para[1];
+      console.log("receive sensor: " + name);
+      var metadata = para[2];
+      var dataTemp, data;
+      try{
+        dataTemp = fs.readFileSync('./sensors.json');
+        data = JSON.parse(dataTemp);
+      } catch(err){
+        console.log('Error loading configration');
+        data = {};
+      }
+      data[name] = {
+                    script: script,
+                    medatada: metadata
+                  };
+      console.log("saving ... " + data);
+      fs.writeFile('./sensors.json', JSON.stringify(data), function (err) {
+      if (err) {
+        handleError(err, -32000, callback);
+        } else{
+          console.log('Configuration saved successfully.');
+          callback(error, data);
+        }
+      });
+    } catch(err){
+      handleError(err, -32000, callback);
+    }
+   } else {
+    handleError(new Error("Invalid params "), -32602, callback);
+   }
+});
+
+serv.addMethod('sensors', function (para, callback) {
+  countTotal ++;
+  console.log('get list of all sensors');
+  var error, result, array = [];
+  try{
+        dataTemp = fs.readFileSync('./sensors.json');
+        data = JSON.parse(dataTemp);
+        for(index in data){
+          console.log('found sensor ' + index);
+          data[index].medatada.name = index;
+          array.push(data[index].medatada);
+          callback(null, array);
+        }
+      } catch(err){
+        handleError(err, -32603, callback)
+      }
+});
+
+serv.addMethod('info', function (para, callback) {
+  countTotal ++;
+  callback(null, {total: countTotal, errors: errorTotal});
 });
 
 // Start the server
