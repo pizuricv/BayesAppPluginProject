76b3ab84d95f3a1b67367ae5be371fa16d5c2b24
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-07-08 13:42:54 +0200

    removed sandbox object from the parking script

--- webscripts/parking_rpc.js
+++ webscripts/parking_rpc.js
@@ -1,25 +1,25 @@
 var url = "http://datatank.gent.be/Mobiliteitsbedrijf/Parkings11.json";
 var error;
-sandbox.console.log("starting parking request");
+console.log("starting parking request");
 value = { observedState: "NotFound"};
-sandbox.request({
+request({
     url: url,
     json: true
 }, function (error, response, body) {
- sandbox.console.log("response code "+response.statusCode);
+ console.log("response code "+response.statusCode);
  if (!error && response.statusCode === 200) {
-    sandbox.console.log("reading locations from: "+body.Parkings11.parkings);
+    console.log("reading locations from: "+body.Parkings11.parkings);
     value = {
        observedState: "Found",
        rawData : {
          locations: body.Parkings11.parkings
        }
     };
-    sandbox.console.log("done, state is: " + value.observedState);
-    sandbox.send(null,value);
+    console.log("done, state is: " + value.observedState);
+    send(null,value);
   } else {
-     sandbox.console.log("error" );
+     console.log("error" );
      var err = { code: response.statusCode, message: response };
-     sandbox.send(err, value);
+     send(err, value);
   }
 });
