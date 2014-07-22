7b0aded8bcb7feba65084df6c1106f2df36a7c28
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-07-22 11:23:11 +0200

    best movies script

---
+++ webscripts/movies.js
@@ -0,0 +1,33 @@
+//var request = require('request');
+//var cheerio = require('cheerio');
+
+var url = "http://www.imdb.com/chart/";
+
+request({
+        "uri": url
+    }, function(err, resp, body){
+        var $ = cheerio.load(body);
+        var movies = [];
+        $('th:contains(Gross)').parents('table').find('tr').each(function(index,item){
+          if(index>0)
+          {
+            var tds = $(item).find('td');
+            var movie = {
+               title: $(tds.eq(1)).find('a').text().trim(),
+               week: tds.eq(2).text().trim(),
+               gross: tds.eq(3).text().trim(),
+               weeks: tds.eq(4).text().trim()
+             };
+             movies.push(movie);
+           }
+       });
+       //console.log(JSON.stringify(movies));
+       var value :{
+        observedState: "Found",
+        rawData :{
+          movies: movies,
+          topMovie: movies[0]
+        }
+       };
+       send(null, value);
+});
