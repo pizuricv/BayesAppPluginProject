0730bd9a120822fb402195fc084699ee09760b97
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-08-28 14:41:18 +0200

    Update bitcoin.js
    fix for JSON parsing

--- webscripts/bitcoin.js
+++ webscripts/bitcoin.js
@@ -2,9 +2,13 @@ var threshold = options.requiredProperties.threshold;
 var API_KEY = options.requiredProperties.key;
 
 unirest.get("https://community-bitcointy.p.mashape.com/average/USD").header("X-Mashape-Key", API_KEY).end(function (result) {
+  var bitcoin = JSON.parse(result.body);
   var value = {
-        observedState: result.body.value < threshold? "Bellow" : "Above",
-        rawData : result.body
+        observedState: bitcoin.value < threshold? "Bellow" : "Above",
+        rawData : {
+            currency: bitcoin.currency,
+            price : bitcoin.value 
+        }
        };
   send(null, value);
 });
