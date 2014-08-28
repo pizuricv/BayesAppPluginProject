f8193541068c0d654d4c45a98d1253633023d2e6
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-08-28 15:13:06 +0200

    added gcm module

--- node/rpc_server.js
+++ node/rpc_server.js
@@ -6,10 +6,12 @@ var Promise = require('promise');
 var vm = require('vm');
 var fs = require('fs');
 var unirest = require('unirest');
+var gcm = require('node-gcm');
+
 
 const ERROR_CODE_NOT_FOUND = 404;
 
-var sandbox = {cheerio:cheerio, request:request, unirest:unirest, console:console};
+var sandbox = {cheerio:cheerio, request:request, gcm: gcm, unirest:unirest, console:console};
 var countTotal = 0;
 var errorTotal = 0;
 
