3b67593e9ace09f8f137749aacf4731c5163e471
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-08-29 14:38:44 +0200

    Update xively_sensor.js

--- webscripts/xively_sensor.js
+++ webscripts/xively_sensor.js
@@ -18,9 +18,17 @@ function callback(error, response, body) {
         data.min_value = parseFloat(data.min_value);
         data.max_value = parseFloat(data.max_value);
         data.threshold = threshold;
+        var raw = {};
+        __.map(var1.datastreams, function(data){
+            raw[data.id+'_current_value'] = parseFloat(data.current_value);
+            raw[data.id+'_min_value'] = parseFloat(data.min_value);
+            raw[data.id+'_max_value'] = parseFloat(data.max_value);
+            //raw[data.id+'_collectedTime'] = new Date(data.at).getTime();
+        });
+        console.log(raw);
         value = {  
             observedState:  data.current_value > threshold ? "Above": "Below", 
-            rawData : data  
+            rawData : raw 
         }; 
         send(null, value);
     }
