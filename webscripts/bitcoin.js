ccab26cbd8792f5c22bd6863bfbdd43bd7f81315
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-08-29 13:01:05 +0200

    another typo...

--- webscripts/bitcoin.js
+++ webscripts/bitcoin.js
@@ -4,7 +4,7 @@ var API_KEY = options.requiredProperties.key;
 unirest.get("https://community-bitcointy.p.mashape.com/average/USD").header("X-Mashape-Key", API_KEY).end(function (result) {
   var bitcoin = JSON.parse(result.body);
   var value = {
-        observedState: bitcoin.value < threshold? "Bellow" : "Above",
+        observedState: bitcoin.value < threshold? "Below" : "Above",
         rawData : {
             currency: bitcoin.currency,
             price : bitcoin.value 
