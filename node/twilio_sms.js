ee828ad8f24c46859f6a8de1a207bde86e0c7e0f
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-09-08 13:48:10 +0200

    added twilio actuator

---
+++ node/twilio_sms.js
@@ -0,0 +1,25 @@
+//var twilio = require('twilio');
+var TWILIO_ACCOUNT_SID = options.requiredProperties.TWILIO_ACCOUNT_SID;
+var TWILIO_AUTH_TOKEN = options.requiredProperties.TWILIO_AUTH_TOKEN;
+var message = options.requiredProperties.message;
+var to = options.requiredProperties.to;
+var from = options.requiredProperties.from;
+ 
+var client = new twilio.RestClient(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
+ 
+// Pass in parameters to the REST API using an object literal notation. The
+// REST client will handle authentication and response serialzation for you.
+client.sms.messages.create({
+    to: to,
+    from: from,
+    body: message
+}, function(error, message) {
+    if (!error) {
+        logger.info('Success! The SID for this SMS message is:');
+        logger.info(message.sid);
+        logger.info('Message sent on:');
+        logger.info(message.dateCreated);
+    } else {
+        logger.error('Oops! There was an error.');
+    }
+});
