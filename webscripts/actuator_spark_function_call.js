7015b63bb8f74c6e4d0942f408fc7cce2905bd13
Francis De Brabandere <francisdb@gmail.com>
2014-09-09 10:41:20 +0200

    added spark core function call actuator

---
+++ webscripts/actuator_spark_function_call.js
@@ -0,0 +1,30 @@
+// name: SparkFunctionCall
+// icon: https://lh4.ggpht.com/wy1RxASYixSM4HGDLV72sTuJN36ycSZaXNions_v3Etu5vWbtmYR8VPAnz3FHcQzFHxG=w300-rw
+// properties:
+//   * access_token
+//   * functionName
+//   * param
+//   * device
+
+var props = options.requiredProperties;
+
+if(props.access_token !== undefined && props.functionName !== undefined && props.param !== undefined && props.device !== undefined){
+
+  var options = {
+    url: 'https://api.spark.io/v1/devices/' + props.device + "/" + props.functionName,
+    form:    { param: props.param, access_token: props.access_token }
+  };
+
+  var callback = function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var bodyJson = JSON.parse(body);
+      send();
+    }else{
+      send(new Error("Calling api.spark.io failed: " + error + " " + body));
+    }
+  };
+
+  request.post(options, callback);
+}else{
+  send(new Error("Missing properties"));
+}
