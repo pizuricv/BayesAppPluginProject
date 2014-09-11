9921e8385fbf8a22344b2958de4cd9dec42474f8
Veselin Pizurica <veselin.pizurica@gmail.com>
2014-09-11 10:46:51 +0200

    added control task actuator

---
+++ webscripts/controlTaskActuator.js
@@ -0,0 +1,13 @@
+var taskID = options.requiredProperties.taskID;
+var command = options.requiredProperties.command;
+
+request.post(
+    'http://app.waylay.io/api/tasks/'+taskID+'/command/'+command,
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            send();
+        } else
+            send(new Error("Missing property testProperty1"));
+    }
+);
+
