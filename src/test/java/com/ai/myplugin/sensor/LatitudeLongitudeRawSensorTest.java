58f6ff415a67c99a9933b38d974c26dfc9991ba7
Francis De Brabandere <francisdb@gmail.com>
2014-11-07 17:19:09 +0100

    re #204 disable more geocoder tests

--- src/test/java/com/ai/myplugin/sensor/LatitudeLongitudeRawSensorTest.java
+++ src/test/java/com/ai/myplugin/sensor/LatitudeLongitudeRawSensorTest.java
@@ -5,12 +5,18 @@
 
 package com.ai.myplugin.sensor;
 
-import junit.framework.TestCase;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.junit.Ignore;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 
-public class LatitudeLongitudeRawSensorTest extends TestCase{
+public class LatitudeLongitudeRawSensorTest{
+
+    @Test
+    @Ignore
+    // see https://www.mashape.com/montanaflynn/geocode-location-lookup/support/5#
     public void testExecute() throws Exception {
         LatitudeLongitudeRawSensor locationSensor = new LatitudeLongitudeRawSensor();
         locationSensor.setProperty("longitude", 19.851858);
