3b2e66d04dca9a953c71067d5c9b4d29a8e6f783
Veselin Pizurica <veselin.pizurica@gmail.com>
2015-01-08 11:52:37 +0100

    remove old scripts

--- webscripts/hello_world.js
+++
@@ -1,6 +0,0 @@
-var randomValue = Math.random(); 
-value = {  
-    observedState:  randomValue > 0.5? "hello" : "world", 
-    rawData : {  message:"message", x: 2.34, random: randomValue, property: options.requiredProperties.testProperty1}  
-}; 
-send(null,value);
